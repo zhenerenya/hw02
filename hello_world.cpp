1079b237a7ec0a424c39f13dd50ab89b104f422c
zhenerenya <zhenyachmchk@gmail.com>
2020-06-03 11:49:35 +0300

    with name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-	cout<<"Hello world!"<<endl;
+	string name;
+	cin>>name;
+	cout<<"Hello world from "<<name<<endl;
 	return 0;
 }
