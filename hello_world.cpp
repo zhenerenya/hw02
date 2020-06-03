4423579f32ce7e2c144dad7865c1953b5e8b2ac9
zhenerenya <zhenyachmchk@gmail.com>
2020-06-03 13:20:30 +0300

    code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 
-
-int main(){
-	//username
-	std::string name;
-	std::cin>>name;
-	//write
-	std::cout<<"Hello world from "<<name<<std::endl;
-	return 0;
+int
+main()
+{
+  //username
+  std::string name;
+  std::cin >> name;
+  //write
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
