717ef4013046822aefae2588dd077a606cbeca22
zhenerenya <zhenyachmchk@gmail.com>
2020-06-03 11:53:25 +0300

    without std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
 
 int main(){
-	string name;
-	cin>>name;
-	cout<<"Hello world from "<<name<<endl;
+	std::string name;
+	std::cin>>name;
+	std::cout<<"Hello world from "<<name<<std::endl;
 	return 0;
 }
