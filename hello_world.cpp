5a9198ba6344dc26cd09eb272352bcbdeab541ed
zhenerenya <zhenyachmchk@gmail.com>
2020-06-03 12:56:56 +0300

    clang format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 
-
-int main(){
-	//имя пользователя
-	std::string name;
-	std::cin>>name;
-	//вывод обращения
-	std::cout<<"Hello world from "<<name<<std::endl;
-	return 0;
+int
+main()
+{
+  //имя пользователя
+  std::string name;
+  std::cin >> name;
+  //вывод обращения
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
